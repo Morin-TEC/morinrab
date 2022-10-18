19adc67571801a90ce15dd64717874a243a02bf2
Morin-TEC <cdmr_le@hotmail.com>
2022-10-17 19:53:07 -0500

    Servidorbd

---
+++ Parcial2/Servidorbd/MySQL.js
@@ -0,0 +1,28 @@
+const express = require('express');
+let mysql      = require('mysql');
+const app = express()
+
+app.use(express.text())
+app.use(express.json())
+
+
+app.get('/:id',(req,res) => {
+
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  port     : '3306',
+  database : 'a19100222'
+})
+connection.connect();
+ 
+connection.query('SELECT * FROM videojuego', function (error, results, fields) {
+  if (error) throw error;
+ // console.log(results);
+  res.send(results)});
+connection.end();
+});
+ 
+app.listen(8082,()=>{
+  console.log('Server express 8082 escuchando correctamente')
+});
